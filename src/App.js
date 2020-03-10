3053bdccec198d817a104b82488c97809b508fba
Nicholas Booth <nicholas.booth@meritec.co.uk>
2020-03-10 16:20:47 +0000

    Add FontAwesome and SASS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faDragon, faBars } from "@fortawesome/free-solid-svg-icons";
+
+import Header from './header'
+import './App.scss';
+
+library.add(
+    faDragon,
+    faBars,
+  );
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className="body-content">
+        <h1>Content!</h1>
+        <p>Here be dragons. Fugiat minim sit reprehenderit ea est.</p>
+      </div>
     </div>
   );
 }
